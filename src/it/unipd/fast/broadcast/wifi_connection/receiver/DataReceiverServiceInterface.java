fde2f3eb89e411385cb7966d203e41478723cd84
Moreno Ambrosin <morambro@gmail.com>
2012-12-08 23:09:55 +0100

    removed deleted files from versioning

--- src/it/unipd/fast/broadcast/wifi_connection/receiver/DataReceiverServiceInterface.java
+++
@@ -1,8 +0,0 @@
-package it.unipd.fast.broadcast.wifi_connection.receiver;
-
-import it.unipd.fast.broadcast.wifi_connection.receiver.DataReceiverService.IDataCollectionHandler;
-
-public interface DataReceiverServiceInterface {
-	void registerHandler(IDataCollectionHandler handler);
-	void unregisterHandler(IDataCollectionHandler handler);
-}
