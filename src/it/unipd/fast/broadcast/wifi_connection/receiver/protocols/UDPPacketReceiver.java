14c47b5014d92676dfe737ad13c1ef953414c07e
Moreno Ambrosin <morambro@gmail.com>
2012-12-13 12:37:12 +0100

    Bella li...

---
+++ src/it/unipd/fast/broadcast/wifi_connection/receiver/protocols/UDPPacketReceiver.java
@@ -0,0 +1,77 @@
+package it.unipd.fast.broadcast.wifi_connection.receiver.protocols;
+
+import it.unipd.fast.broadcast.wifi_connection.message.IMessage;
+import it.unipd.fast.broadcast.wifi_connection.message.MessageBuilder;
+import it.unipd.fast.broadcast.wifi_connection.receiver.AbstractPacketReceiver;
+import it.unipd.fast.broadcast.wifi_connection.receiver.IDataReceiverService.IDataCollectionHandler;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+import android.util.Log;
+
+public class UDPPacketReceiver extends AbstractPacketReceiver {
+	protected final String TAG = "it.unipd.fast.broadcast";
+	
+	protected final int PACKET_SIZE = 1024;
+	protected int server_port = 8888;
+	
+	protected DatagramSocket socket;
+	
+	@Override
+	public void run() {
+		try{
+			socket = new DatagramSocket(server_port);
+			final byte[] message = new byte[PACKET_SIZE];
+			final DatagramPacket p = new DatagramPacket(message, message.length);
+			while(!terminated){
+				
+				socket.receive(p);
+				new Thread(){
+					public void run(){
+						String xmlMsg = new String(message, 0, p.getLength());
+						IMessage xmlMessage = MessageBuilder.getInstance().getMessage(xmlMsg);
+						
+						Log.d(TAG,UDPPacketReceiver.class.getSimpleName() +"message : " + xmlMsg);
+						Log.d(TAG,UDPPacketReceiver.class.getSimpleName() +"handlers : " + handlers);
+						
+						for (IDataCollectionHandler handler : handlers) {
+							Log.d(TAG,"Calling handler ");
+							handler.onDataCollected(
+								xmlMessage ,
+								xmlMessage .getContent().get(IMessage.SENDER_IP_ADDR)
+							);
+						}
+					}
+				}.start();
+			}
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		finally{
+			disconnectSocket();
+		}
+	}
+
+	@Override
+	public void terminate() {
+		this.terminated = false;
+	}
+	
+	/**
+	 * Method called to close the opened TCP socket if different to null and opened
+	 * 
+	 */
+	private void disconnectSocket(){
+		if(socket != null && !socket.isClosed()){
+			try{
+				socket.close();
+				Log.d(TAG, this.getClass().getSimpleName()+": Ho chiuso il socket");
+			}catch(Exception e){
+				Log.d(TAG, this.getClass().getSimpleName()+": Impossibile chiudere il socket");
+			}
+		}
+	}
+
+}
